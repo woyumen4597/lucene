1134648098beb4471f90adcddac39487ed8b48e4
jrc <woyumen4597@gmail.com>
2018-01-24 19:21:42 +0800

    Add AC automaton

---
+++ src/main/java/cn/jrc/spider/rmdup/interval/IntervalableComparatorByPosition.java
@@ -0,0 +1,15 @@
+package cn.jrc.spider.rmdup.interval;
+
+import java.util.Comparator;
+
+/**
+ * 按起点比较区间
+ */
+public class IntervalableComparatorByPosition implements Comparator<Intervalable>
+{
+    @Override
+    public int compare(Intervalable intervalable, Intervalable intervalable2)
+    {
+        return intervalable.getStart() - intervalable2.getStart();
+    }
+}
