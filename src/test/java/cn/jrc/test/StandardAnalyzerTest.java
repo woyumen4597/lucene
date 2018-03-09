ee78e393c7dcabbc396eaf6901dcf734dbb1e989
jrc <woyumen4597@gmail.com>
2018-03-09 17:38:29 +0800

    Start index test.

--- src/test/java/cn/jrc/test/StandardAnalyzerTest.java
+++ src/test/java/cn/jrc/test/StandardAnalyzerTest.java
@@ -3,6 +3,9 @@ package cn.jrc.test;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 import org.apache.lucene.search.highlight.*;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
@@ -18,12 +21,28 @@ public class StandardAnalyzerTest {
     public static void analyze(Analyzer analyzer,String text) throws IOException, InvalidTokenOffsetsException {
         System.out.println("分词器: "+analyzer.getClass());
         TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(text));
-        tokenStream.addAttribute(CharTermAttribute.class);
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);//增加Token表示的字符串属性
+        OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
+        PositionIncrementAttribute positionIncrementAttribute = tokenStream.addAttribute(PositionIncrementAttribute.class);
+        TypeAttribute typeAttribute = tokenStream.addAttribute(TypeAttribute.class);
         tokenStream.reset();
+        int position = 0;
         while(tokenStream.incrementToken()){
             CharTermAttribute attribute = tokenStream.getAttribute(CharTermAttribute.class);
-            System.out.println(attribute.toString());
+            System.out.println("输出结果: "+attribute.toString());
+            int startOffset = offsetAttribute.startOffset();
+            int endOffset = offsetAttribute.endOffset();
+            System.out.println("start:"+startOffset+" end:"+endOffset);
+
+            int increment = positionIncrementAttribute.getPositionIncrement();
+            if(increment>0){
+                position = position+increment;
+            }
+            System.out.println("第"+position+"个分词");
+            System.out.println("类型:"+typeAttribute.type());
+            System.out.println("------------");
         }
+        tokenStream.close();
     }
 
     public static void main(String[] args) throws IOException, InvalidTokenOffsetsException {
